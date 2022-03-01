7f50a4c2780232ebd436c0d3a06b00877e65ff1b
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-03-01 19:11:49 -0330

    validate post route

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+const fs = require('fs');
+const path = require('path'); //provides utilities for working with file and directory paths 
 const express = require('express');
 const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 
 // Filter functionality
@@ -45,7 +52,38 @@ function filterByQuery(query, animalArray) {
 function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
-}
+};
+
+// function that accepts POST routes req.body value and the array we ant to add the data to 
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+    );    
+
+    // return finished code to post route for response
+    return animal;
+};
+
+// Validate user data
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+        return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+        return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+        return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+        return false;
+    }
+    return true;
+
+};
 
 
 app.get('/api/animals', (req, res) => {
@@ -62,6 +100,20 @@ app.get('/api/animals/:id', (req, res) => {
         res.json(result);
     } else{
         res.send(404);
+    }
+});
+
+// Accept data to be used or stored server side
+app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be 
+    req.body.id = animals.length.toString();
+
+
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+        const animal = createNewAnimal(req.body, animals);
+        res.json(animal);
     }
 });
 
